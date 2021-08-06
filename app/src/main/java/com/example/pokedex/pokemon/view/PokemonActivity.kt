41c0e27c096e8fe48f1f3c427d1951a23826f149
RamziJabali <ramzieljabali@gmail.com>
2021-08-05 19:17:27 -0700

    Made changes to PokemonActivity and added Stat bar for Pokemons

--- app/src/main/java/com/example/pokedex/pokemon/view/PokemonActivity.kt
+++ app/src/main/java/com/example/pokedex/pokemon/view/PokemonActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.pokedex.R
@@ -34,31 +35,37 @@ class PokemonActivity : AppCompatActivity(), ViewListener {
     private var pokemonId: Int = -1
     private val pokemonViewModel: PokemonViewModel by viewModel()
 
-    private lateinit var pokemonEnhancedImageView: ImageView
+    private val pokemonEnhancedImageView: ImageView by lazy { findViewById(R.id.pokemonImageEnhanced) }
 
-    private lateinit var pokemonType1: TextView
-    private lateinit var pokemonType2: TextView
+    private val pokemonType1: TextView by lazy { findViewById(R.id.pokemonTypeOneTextView) }
+    private val pokemonType2: TextView by lazy { findViewById(R.id.pokemonTypeTwoTextView) }
 
-    private lateinit var pokemonStat1Name: TextView
-    private lateinit var pokemonStat2Name: TextView
-    private lateinit var pokemonStat3Name: TextView
-    private lateinit var pokemonStat4Name: TextView
-    private lateinit var pokemonStat5Name: TextView
-    private lateinit var pokemonStat6Name: TextView
+    private val pokemonStat1Name: TextView by lazy { findViewById(R.id.statOne) }
+    private val pokemonStat2Name: TextView by lazy { findViewById(R.id.statTwo) }
+    private val pokemonStat3Name: TextView by lazy { findViewById(R.id.statThree) }
+    private val pokemonStat4Name: TextView by lazy { findViewById(R.id.statFour) }
+    private val pokemonStat5Name: TextView by lazy { findViewById(R.id.statFive) }
+    private val pokemonStat6Name: TextView by lazy { findViewById(R.id.statSix) }
 
-    private lateinit var pokemonStat1Value: TextView
-    private lateinit var pokemonStat2Value: TextView
-    private lateinit var pokemonStat3Value: TextView
-    private lateinit var pokemonStat4Value: TextView
-    private lateinit var pokemonStat5Value: TextView
-    private lateinit var pokemonStat6Value: TextView
+    private val pokemonStat1Value: TextView by lazy { findViewById(R.id.statOneValue) }
+    private val pokemonStat2Value: TextView by lazy { findViewById(R.id.statTwoValue) }
+    private val pokemonStat3Value: TextView by lazy { findViewById(R.id.statThreeValue) }
+    private val pokemonStat4Value: TextView by lazy { findViewById(R.id.statFourValue) }
+    private val pokemonStat5Value: TextView by lazy { findViewById(R.id.statFiveValue) }
+    private val pokemonStat6Value: TextView by lazy { findViewById(R.id.statSixValue) }
+
+    private val pokemonStatBar1: ProgressBar by lazy { findViewById(R.id.horizontal_loading_bar_1) }
+    private val pokemonStatBar2: ProgressBar by lazy { findViewById(R.id.horizontal_loading_bar_2) }
+    private val pokemonStatBar3: ProgressBar by lazy { findViewById(R.id.horizontal_loading_bar_3) }
+    private val pokemonStatBar4: ProgressBar by lazy { findViewById(R.id.horizontal_loading_bar_4) }
+    private val pokemonStatBar5: ProgressBar by lazy { findViewById(R.id.horizontal_loading_bar_5) }
+    private val pokemonStatBar6: ProgressBar by lazy { findViewById(R.id.horizontal_loading_bar_6) }
 
     private val compositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pokemon)
-        setupFragmentMembers()
         pokemonId = intent.getIntExtra(EXTRA_POKEMON_ID, -1)
         if (pokemonId == -1) {
             throw IllegalStateException("Pokemon ID is not found")
@@ -66,42 +73,22 @@ class PokemonActivity : AppCompatActivity(), ViewListener {
         pokemonViewModel.getInformation(pokemonId)
 
         compositeDisposable.add(pokemonViewModel.pokemonViewStateObservable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        { pokemonViewState -> setFragmentMembers(pokemonViewState) },
-                        { error -> Log.e(TAG, error.localizedMessage ?: "Error") }
-                ))
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { pokemonViewState -> setFragmentMembers(pokemonViewState) },
+                { error -> Log.e(TAG, error.localizedMessage ?: "Error") }
+            ))
     }
 
     override fun setNewViewState(pokemonViewState: PokemonViewState) {
         setFragmentMembers(pokemonViewState)
     }
 
-    private fun setupFragmentMembers() {
-        pokemonType1 = findViewById(R.id.pokemonTypeOneTextView)
-        pokemonType2 = findViewById(R.id.pokemonTypeTwoTextView)
-        pokemonStat1Name = findViewById(R.id.statOne)
-        pokemonStat2Name = findViewById(R.id.statTwo)
-        pokemonStat3Name = findViewById(R.id.statThree)
-        pokemonStat4Name = findViewById(R.id.statFour)
-        pokemonStat5Name = findViewById(R.id.statFive)
-        pokemonStat6Name = findViewById(R.id.statSix)
-
-        pokemonStat1Value = findViewById(R.id.statOneValue)
-        pokemonStat2Value = findViewById(R.id.statTwoValue)
-        pokemonStat3Value = findViewById(R.id.statThreeValue)
-        pokemonStat4Value = findViewById(R.id.statFourValue)
-        pokemonStat5Value = findViewById(R.id.statFiveValue)
-        pokemonStat6Value = findViewById(R.id.statSixValue)
-
-        pokemonEnhancedImageView = findViewById(R.id.pokemonImageEnhanced)
-    }
-
     private fun setFragmentMembers(pokemonViewState: PokemonViewState) {
         Picasso.get()
-                .load("https://pokeres.bastionbot.org/images/pokemon/${pokemonId}.png")
-                .into(pokemonEnhancedImageView)
+            .load("https://pokeres.bastionbot.org/images/pokemon/${pokemonId}.png")
+            .into(pokemonEnhancedImageView)
         pokemonType1.text = pokemonViewState.pokemonType[0].pokemonSpecificType.pokemonTypeName
 
         if (pokemonViewState.pokemonType.size > 1) {
@@ -113,12 +100,24 @@ class PokemonActivity : AppCompatActivity(), ViewListener {
         pokemonStat4Name.text = pokemonViewState.pokemonStats[3].pokemonStatType.statTypeName
         pokemonStat5Name.text = pokemonViewState.pokemonStats[4].pokemonStatType.statTypeName
         pokemonStat6Name.text = pokemonViewState.pokemonStats[5].pokemonStatType.statTypeName
+
         pokemonStat1Value.text = pokemonViewState.pokemonStats[0].pokemonBaseStat.toString()
+        pokemonStatBar1.progress = pokemonViewState.pokemonStats[0].pokemonBaseStat
+
         pokemonStat2Value.text = pokemonViewState.pokemonStats[1].pokemonBaseStat.toString()
+        pokemonStatBar2.progress = pokemonViewState.pokemonStats[1].pokemonBaseStat
+
         pokemonStat3Value.text = pokemonViewState.pokemonStats[2].pokemonBaseStat.toString()
+        pokemonStatBar3.progress = pokemonViewState.pokemonStats[2].pokemonBaseStat
+
         pokemonStat4Value.text = pokemonViewState.pokemonStats[3].pokemonBaseStat.toString()
+        pokemonStatBar4.progress = pokemonViewState.pokemonStats[3].pokemonBaseStat
+
         pokemonStat5Value.text = pokemonViewState.pokemonStats[4].pokemonBaseStat.toString()
+        pokemonStatBar5.progress = pokemonViewState.pokemonStats[4].pokemonBaseStat
+
         pokemonStat6Value.text = pokemonViewState.pokemonStats[5].pokemonBaseStat.toString()
+        pokemonStatBar6.progress = pokemonViewState.pokemonStats[5].pokemonBaseStat
     }
 }
 
