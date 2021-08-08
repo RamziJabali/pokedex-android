ec5b714555b584d1ce00f83fd4903d116d1aa316
RamziJabali <ramzieljabali@gmail.com>
2021-08-08 14:31:53 -0700

    Pokemon page Junit test

--- app/src/androidTest/java/com/example/pokedex/PokemonTest.kt
+++ app/src/androidTest/java/com/example/pokedex/PokemonTest.kt
@@ -6,6 +6,7 @@ import com.example.pokedex.pokemon.view.PokemonActivity
 import io.mockk.mockk
 import org.junit.Assert.assertTrue
 import org.junit.Before
+import org.junit.Test
 
 class PokemonTest {
 
@@ -13,11 +14,71 @@ class PokemonTest {
     private val pokemonActivity: PokemonActivity by lazy { mockk(relaxed = true) }
 
     @Before
-    fun setup(){
+    fun setup() {
         pokemonActivity.pokemonViewModel.getInformation(mainActivity.viewState.gridProperties[0].itemOrderNumber)
     }
 
-    fun pokemonTypeTest(){
+    @Test
+    fun pokemonTypeTest() {
         assertTrue(pokemonActivity.pokemonType1.text.isNotEmpty())
+    }
+
+    @Test
+    fun pokemonStat1NameTest() {
+        assertTrue(pokemonActivity.pokemonStat1Name.text.equals("HP"))
+    }
+
+    @Test
+    fun pokemonStat2NameTest() {
+        assertTrue(pokemonActivity.pokemonStat2Name.text.equals("ATTACK"))
+    }
+
+    @Test
+    fun pokemonStat3NameTest() {
+        assertTrue(pokemonActivity.pokemonStat3Name.text.equals("DEFENSE"))
+    }
+
+    @Test
+    fun pokemonStat4NameTest() {
+        assertTrue(pokemonActivity.pokemonStat4Name.text.equals("SPECIAL-ATTACK"))
+    }
+
+    @Test
+    fun pokemonStat5NameTest() {
+        assertTrue(pokemonActivity.pokemonStat5Name.text.equals("SPECIAL-DEFENSE"))
+    }
+
+    @Test
+    fun pokemonStat6NameTest() {
+        assertTrue(pokemonActivity.pokemonStat6Name.text.equals("SPEED"))
+    }
+
+    @Test
+    fun pokemonStat1ValueTest() {
+        assertTrue(pokemonActivity.pokemonStat1Value.text.isNotBlank())
+    }
+
+    @Test
+    fun pokemonStat2ValueTest() {
+        assertTrue(pokemonActivity.pokemonStat2Value.text.isNotBlank())
+    }
+
+    @Test
+    fun pokemonStat3ValueTest() {
+        assertTrue(pokemonActivity.pokemonStat3Value.text.isNotBlank())
+    }
+
+    @Test
+    fun pokemonStat4ValueTest() {
+        assertTrue(pokemonActivity.pokemonStat4Value.text.isNotBlank())
+    }
+
+    @Test
+    fun pokemonStat5ValueTest() {
+        assertTrue(pokemonActivity.pokemonStat5Value.text.isNotBlank())
+    }
+    @Test
+    fun pokemonStat6ValueTest() {
+        assertTrue(pokemonActivity.pokemonStat6Value.text.isNotBlank())
     }
 }
